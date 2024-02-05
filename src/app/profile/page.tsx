447f2ebbeaeae59b9e49fac93c17710679a23740
qyj <1078869122@qq.com>
2024-02-05 20:51:00 +0800

    first commit

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+export default function Profile() {
+  return (
+    <main className={styles.main}>
+      <div>this is profile page</div>
+    </main>
+  )
+}
