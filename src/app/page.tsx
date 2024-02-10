e3d10a22ceca58db99c8e8174dba76fb34bf0f64
qyj <1078869122@qq.com>
2024-02-10 21:57:47 +0800

    build: without data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,23 @@ import './page.scss'
 import Notification from '~/components/Home/Notification'
 import Banner from '~/components/Home/Banner'
 
-async function getData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api`)
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-  return res.json()
-}
+// async function getData() {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api`)
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
+//   return res.json()
+// }
 
 export default async function Home() {
-  const data = await getData()
+  // const data = await getData()
 
   return (
     <main className='main'>
       {/* <ThemeChange></ThemeChange> */}
       <Notification></Notification>
       <Banner>
-        <div>{data.hello}</div>
+        {/* <div>{data.hello}</div> */}
         <div>BEST SELLER</div>
         <div>BEST DISPENSARY TO BUY WEED ONLINE</div>
         <div>Vitamins & Supplements</div>
