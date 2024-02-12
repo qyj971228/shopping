57896dc93ac3f60e974d9982ff28665f0b0bbf14
qyj <1078869122@qq.com>
2024-02-12 22:10:16 +0800

    feat: home banner

--- src/components/pages/Home/Banner.tsx
+++ src/components/pages/Home/Banner.tsx
@@ -1,6 +1,10 @@
 import './Banner.scss'
 import Image from 'next/image'
 import BannerBg from '@/img/conic-gradient.png'
+import Banner1 from '@/img/banner1.png'
+import Banner2 from '@/img/banner2.png'
+import Banner3 from '@/img/banner3.png'
+import Cubic from './Cubic'
 
 export default function Banner() {
   return (
@@ -11,19 +15,68 @@ export default function Banner() {
         alt='banner-bg'
         placeholder='blur'
       />
-      <div className='children'>
+      <div className='content'>
         <div className='left-part'>
-          <span className='subtitle'>BEST SELLER</span>
-          <span className='title'>BEST DISPENSARY TO BUY WEED ONLINE</span>
-          <span className='describes'>Vitamins & Supplements</span>
-          <span className='discount'>
+          <div className='subtitle'>BEST SELLER</div>
+          <div className='title'>BEST DISPENSARY TO BUY WEED ONLINE</div>
+          <div className='describes'>Vitamins & Supplements</div>
+          <div className='discount'>
             <span>Get 25% off</span>
             <div className='vertical-line'></div>
             <span>Free Shipping</span>
-          </span>
-          <button>Shop All</button>
+          </div>
+          <button className='shop-all'>Shop All</button>
         </div>
-        <div className='right-part'></div>
+        <div className='right-part'>
+          <div className='cubics'>
+            <Cubic
+              src={Banner2}
+              trans={{ x: 200, y: 250 }}
+              cubicWidth={296}
+              bannerWidth={172}
+              bannerTransY={100}
+              style={{zIndex: 1}}
+            ></Cubic>
+            <Cubic
+              src={Banner1}
+              trans={{ x: 145, y: 0 }}
+              cubicWidth={296}
+              bannerWidth={211}
+              bannerTransY={100}
+            ></Cubic>
+            <Cubic
+              src={Banner3}
+              trans={{ x: 0, y: 430 }}
+              cubicWidth={436}
+              bannerWidth={147}
+              bannerTransY={100}
+            ></Cubic>
+          </div>
+          <div className='cubics-mini'>
+            <Cubic
+              src={Banner2}
+              trans={{ x: 100, y: 125 }}
+              cubicWidth={143}
+              bannerWidth={88}
+              bannerTransY={50}
+              style={{zIndex: 1}}
+            ></Cubic>
+            <Cubic
+              src={Banner1}
+              trans={{ x: 83, y: 40 }}
+              cubicWidth={170}
+              bannerWidth={108}
+              bannerTransY={40}
+            ></Cubic>
+            <Cubic
+              src={Banner3}
+              trans={{ x: 0, y: 165 }}
+              cubicWidth={180}
+              bannerWidth={75}
+              bannerTransY={50}
+            ></Cubic>
+          </div>
+        </div>
       </div>
     </div>
   )
---
+++ src/components/pages/Home/Cubic.tsx
@@ -0,0 +1,42 @@
+import './Cubic.scss'
+import Image, { StaticImageData } from 'next/image'
+import CubicImg from '@/img/isometric-cubic.png'
+import { CSSProperties } from 'react'
+
+export default function Cubic({
+  src,
+  bannerWidth,
+  bannerTransY,
+  cubicWidth,
+  trans,
+  style,
+}: {
+  src: StaticImageData
+  bannerWidth: number
+  bannerTransY: number
+  cubicWidth: number
+  trans: { x: number; y: number }
+  style?: CSSProperties
+}) {
+  const bannerCSS = {
+    width: String(bannerWidth) + 'px',
+    height: String(bannerWidth) + 'px',
+    transform: `translateY(${bannerTransY}px)`,
+  } as CSSProperties
+
+  const cubicCSS = {
+    width: String(cubicWidth) + 'px',
+    height: String(cubicWidth * 1.75) + 'px',
+  } as CSSProperties
+
+  const comCSS = {
+    transform: `translate(${trans.x}px, ${trans.y}px)`,
+  }
+
+  return (
+    <div className='cubic' style={{ ...style, ...comCSS }}>
+      <Image className='banner-img' style={bannerCSS} src={src} alt='banner' />
+      <Image style={cubicCSS} src={CubicImg} alt='cubic' />
+    </div>
+  )
+}
