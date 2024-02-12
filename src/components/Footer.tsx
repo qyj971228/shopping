9a3e38a09d67a8611937f11dfeedba2f43362ff9
qyj <1078869122@qq.com>
2024-02-12 22:34:27 +0800

    feat: footer disclaimer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 
 export default function Footer() {
   return <footer>
-    this is the footer
+    Disclaimer: This website is for educational purposes only and does not represent personal views or political positions.
   </footer>
 }
