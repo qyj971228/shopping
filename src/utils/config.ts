0b909f7ca443a50000f7f3bc9256d6b27a12d3cb
qyj <1078869122@qq.com>
2024-02-17 03:04:49 +0800

    feat: always use locale en

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,14 +3,14 @@ import { Pathnames } from 'next-intl/navigation'
 export const locales = ['en', 'zh'] as const
 
 export const pathnames = {
-  '/': '/',
-  '/profile': {
-    en: '/profile',
-    zh: '/profile',
-  },
+  // '/': '/',
+  // '/profile': {
+  //   en: '/profile',
+  //   zh: '/profile',
+  // },
 } satisfies Pathnames<typeof locales>
 
-// Use the default: `always`
-export const localePrefix = undefined
+// Use the default: `always`; auto: `undefined`
+export const localePrefix = 'always'
 
 export type AppPathnames = keyof typeof pathnames
