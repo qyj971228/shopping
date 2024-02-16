c37ae2c91b45da87b75e1d6420200efa7c6408e3
qyj <1078869122@qq.com>
2024-02-17 03:09:13 +0800

    fix: link href type

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,11 +3,11 @@ import { Pathnames } from 'next-intl/navigation'
 export const locales = ['en', 'zh'] as const
 
 export const pathnames = {
-  // '/': '/',
-  // '/profile': {
-  //   en: '/profile',
-  //   zh: '/profile',
-  // },
+  '/': '/',
+  '/profile': {
+    en: '/profile',
+    zh: '/ge-ren-xin-xi',
+  },
 } satisfies Pathnames<typeof locales>
 
 // Use the default: `always`; auto: `undefined`
